702970184e3a044cdc794e813ade21c6b062d4bd
Billy Kim <billyhkim.dev@gmail.com>
2019-12-15 03:33:14 -0800

    added root reducer

--- client/reducers/todoReducer.js
+++ client/reducers/todoReducer.js
@@ -1,15 +1,43 @@
-import { FETCH_TODOS, NEW_TODO } from '../actions/types';
+import { FETCH_TODOS, NEW_TODO, DELETE_TODO, TOGGLE_TODO } from '../actions/types';
 
 const initialState = {
-  // from FETCH_TODOS
   todos: [],
-  // to store the single todo
-  todo: {}
 };
 
 // action must include type
 export default (state = initialState, action) => {
   switch (action.type) {
+    case FETCH_TODOS:
+      console.log('todoReducer');
+      return {
+        // ...state is the current state
+        ...state,
+        todos: action.payload
+      };
+    case NEW_TODO:
+      return {
+        ...state,
+        todos: [...state.todos, action.payload]
+      };
+    case DELETE_TODO:
+      return {
+        todos: [...state.todos.filter(todo => todo.id !== action.payload)]
+      };
+    case TOGGLE_TODO:
+      return {
+        todos: [...state.todos.map(todo => {
+          if (todo.id === action.payload) {
+            return {
+              todo: todo.todo,
+              completed: !todo.completed,
+              description: todo.description,
+              id: todo.id,
+            }
+          } else {
+            return todo;
+          }
+        })]
+      };
     default:
       return state;
   }
