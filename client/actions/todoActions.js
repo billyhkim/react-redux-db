24a7bd71c254a21e316d4c72ccea60209a481422
Billy Kim <billyhkim.dev@gmail.com>
2019-12-15 03:34:16 -0800

    added all actions and axios requests

--- client/actions/todoActions.js
+++ client/actions/todoActions.js
@@ -1,7 +1,61 @@
-import { FETCH_TODOS, NEW_TODO } from './types';
+import axios from 'axios';
+import { FETCH_TODOS, NEW_TODO, DELETE_TODO, TOGGLE_TODO } from './types';
 
-export function fetchTodos() {
-  return function (dispatch) {
+export const fetchTodos = () => dispatch => {
+  console.log('fetchTodos');
+  axios.get('/api/todos')
+    // .then(res => console.log(res.data))
+    .then(res => dispatch({
+      type: FETCH_TODOS,
+      payload: res.data
+    }));
+}
 
+export const newTodo = (todoData) => dispatch => {
+  console.log('newTodo');
+  const postConfig = {
+    method: 'POST',
+    url: '/api/todos',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: JSON.stringify(todoData),
   };
+  axios(postConfig)
+    .then(res => dispatch({
+      type: NEW_TODO,
+      payload: res.data
+    }));
+}
+
+export const deleteTodo = (todoId) => dispatch => {
+  console.log('deleteTodo');
+  const deleteConfig = {
+    method: 'DELETE',
+    url: `/api/todos/${todoId}`,
+  };
+  axios(deleteConfig)
+    .then(res => dispatch({
+      type: DELETE_TODO,
+      payload: todoId
+    }));
+}
+
+export const toggleTodo = (todoId, completedStatus) => dispatch => {
+  console.log('toggleTodo');
+  const toggleConfig = {
+    method: 'PATCH',
+    url: `/api/todos/${todoId}`,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: JSON.stringify({
+      completed: !completedStatus
+    })
+  };
+  axios(toggleConfig)
+    .then(res => dispatch({
+      type: TOGGLE_TODO,
+      payload: todoId
+    }));
 }
