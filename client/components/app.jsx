6c06da98b738d90ba10fa9fffddfe4470095c87c
Billy Kim <billyhkim.dev@gmail.com>
2019-12-15 03:32:21 -0800

    connected redux to app.jsx

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import Header from './header';
 import TodoList from './todo-list';
+import TodoInput from './todo-input';
 import { Provider } from 'react-redux';
+import { Row, Col } from 'reactstrap';
 import store from '../store';
 
 class App extends React.Component {
@@ -9,7 +11,14 @@ class App extends React.Component {
     return (
       <Provider store={store}>
         <Header />
-        <TodoList />
+        <Row>
+          <Col xs="8">
+            <TodoList />
+          </Col>
+          <Col xs="4">
+            <TodoInput />
+          </Col>
+        </Row>
       </Provider>
     );
   }
